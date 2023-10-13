4426ef57b0d3f91a30a92962e44d8a18bc4eaa52
Rasik Abdullah <rasikabdullah@gmail.com>
2023-10-14 02:31:01 +0600

    update style

--- script.js
+++ script.js
@@ -10,46 +10,46 @@
         if (board[row][col] === '' && !checkWinner()) {
           const cell = document.querySelector(`table tr:nth-child(${row + 1}) td:nth-child(${col + 1})`);
           cell.textContent = currentPlayer;
-          cell.style.color = 'white'; // Highlight the cell
+          cell.style.color = 'white';
 
           board[row][col] = currentPlayer;
 
-          // Play a sound for each move
+
           const audio = new Audio('/move.mp3');
           audio.play();
 
           if (checkWinner()) {
             document.getElementById('result').textContent = `Player ${currentPlayer} wins!`;
 
-            // Play a sound for winning
+
             const winAudio = new Audio('./sounds/win.mp3');
             winAudio.play();
 
-            // Show an alert and restart the game
+
             setTimeout(() => {
               alert(`Player ${currentPlayer} wins!`);
-              resetGame(); // Start a new game
-            }, 500);
+              resetGame(); 
+            }, 750);
           } else if (isBoardFull()) {
-            // The game is a tie
+
             document.getElementById('result').textContent = "It's a draw!";
 
-            // Play a sound for a tie
+
             const tieAudio = new Audio('./sounds/draw.mp3');
             tieAudio.play();
 
-            // Show an alert and restart the game
+
             setTimeout(() => {
               alert("It's a draw!");
-              resetGame(); // Start a new game
-            }, 500);
+              resetGame();
+            }, 750);
           } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             const turnMessage = document.getElementById('turnMessage');
             turnMessage.textContent = `Now it's ${currentPlayer}'s turn`;
           }
         } else {
-          // Play a sound for each valid or invalid move
+
           const audio = new Audio('/move.mp3');
           audio.play();
         }
@@ -58,7 +58,7 @@
       function checkWinner() {
         for (let i = 0; i < 3; i++) {
           if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
-            // Highlight the winning line
+
             for (let j = 0; j < 3; j++) {
               const cell = document.querySelector(`table tr:nth-child(${i + 1}) td:nth-child(${j + 1})`);
               cell.classList.add('winning-line');
@@ -66,7 +66,7 @@
             return true;
           }
           if (board[0][i] !== '' && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
-            // Highlight the winning line
+
             for (let j = 0; j < 3; j++) {
               const cell = document.querySelector(`table tr:nth-child(${j + 1}) td:nth-child(${i + 1})`);
               cell.classList.add('winning-line');
@@ -76,7 +76,7 @@
         }
 
         if (board[0][0] !== '' && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-          // Highlight the winning line
+
           for (let i = 0; i < 3; i++) {
             const cell = document.querySelector(`table tr:nth-child(${i + 1}) td:nth-child(${i + 1})`);
             cell.classList.add('winning-line');
@@ -84,7 +84,7 @@
           return true;
         }
         if (board[0][2] !== '' && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
-          // Highlight the winning line
+
           for (let i = 0; i < 3; i++) {
             const cell = document.querySelector(`table tr:nth-child(${i + 1}) td:nth-child(${2 - i + 1})`);
             cell.classList.add('winning-line');
@@ -95,7 +95,7 @@
       }
 
       function isBoardFull() {
-        // Check if the board is full
+
         for (let i = 0; i < 3; i++) {
           for (let j = 0; j < 3; j++) {
             if (board[i][j] === '') {
@@ -107,11 +107,11 @@
       }
 
       function resetGame() {
-        // Clear the board and start a new game
+
         board = [['', '', ''], ['', '', ''], ['', '', '']];
         currentPlayer = 'X';
 
-        // Reset cell contents and styles
+
         const cells = document.querySelectorAll('table td');
         cells.forEach(cell => {
           cell.textContent = '';
